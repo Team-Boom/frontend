0dda9eb282c64b947eed6d7198911dc5e9071df7
Tasha R Zuniga <Tashazun@gmail.com>
2018-06-23 14:18:31 -0700

    basic component set-up

---
+++ src/components/App/App.js
@@ -0,0 +1,32 @@
+import React, { PureComponent } from 'react';
+// import PropTypes from 'prop-types';
+// import connect from 'react-redux';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+// import { tryLoadUser } from '../auth/actions';
+// import { getCheckedAuth } from '../auth/reducers';
+import Home from '../Home/Home';
+import Auth from '../auth/Auth';
+import Nav from '../Nav/Nav';
+
+class App extends PureComponent {
+  
+  render() {
+
+    return (
+      <Router>
+        <main>
+          <Nav/>
+          <div> 
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route path="/auth" component={Auth}/>
+              <Redirect to="/"/>
+            </Switch>
+          </div>
+        </main>
+      </Router>
+    );
+  }
+}
+
+export default App;
