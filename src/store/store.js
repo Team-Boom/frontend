e3890f0ad7e692bffbcd134e5d9e784e27d518bd
Jen Lipton <jkl.lipton@gmail.com>
2018-06-23 16:46:31 -0700

    updated store

--- src/store/store.js
+++ src/store/store.js
@@ -1,21 +1,31 @@
-// import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-// import thunk from 'redux-thunk';
-// // import promiseMiddleware from './promise-middleware';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { reviewsById } from '../components/reviews/reducers';
+import { user, checkedAuth } from '../components/profile/reducers';
+import { search, results } from '../components/nav/reducers';
+import { movie, top10s } from '../components/movies/reducers';
+import thunk from 'redux-thunk';
+import promiseMiddleware from './promise-middleware';
 
-// const rootReducer = combineReducers({
-//  //inputs
-// });
+const rootReducer = combineReducers({
+  user,
+  checkedAuth,
+  reviewsById,
+  search,
+  results,
+  movie,
+  top10s
+});
 
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-// const store = createStore(
-//   rootReducer,
-//   composeEnhancers(
-//     applyMiddleware(
-//       thunk,
-//     //   promiseMiddleware
-//     )
-//   )
-// );
+const store = createStore(
+  rootReducer,
+  composeEnhancers(
+    applyMiddleware(
+      thunk,
+      promiseMiddleware
+    )
+  )
+);
 
-// export default store;
+export default store;
