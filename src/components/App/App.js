d2fe69743193546b9948a5d479f7b0510b279efb
Tasha R Zuniga <Tashazun@gmail.com>
2018-06-25 11:38:19 -0700

    first draft routes and auth setup, waiting for api key to adjust fetch requests

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,11 @@
 import React, { PureComponent } from 'react';
-// import PropTypes from 'prop-types';
-// import connect from 'react-redux';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { tryLoadUser } from '../auth/actions';
+import { getCheckedAuth } from '../auth/reducers';
 import Auth from '../auth/Auth';
+import PrivateRoute from './PrivateRoutes';
 import Home from '../home/Home';
 import Nav from '../nav/Nav';
 import Browse from '../browse/Browse';
@@ -13,18 +16,36 @@ import Reviews from '../review/Reviews';
 
 class App extends PureComponent {
   
+  static propTypes = {
+    tryLoadUser: PropTypes.func.isRequired,
+    checkedAuth: PropTypes.bool.isRequired
+  };
+
+  componentDidMount() {
+    this.props.tryLoadUser();
+  }
+
   render() {
+
+    const { checkedAuth } = this.props;
 
     return (
       <Router>
         <main>
           <Nav/>
-          <div> 
+          <div>
+            { checkedAuth &&
             <Switch>
               <Route exact path="/" component={Home}/>
               <Route path="/auth" component={Auth}/>
+              <Route path="/browse" component={Browse}/>
+              <Route path="/movies" component={Movies}/>
+              <PrivateRoute path="/profile" component={Profile}/>
+              <PrivateRoute path="/watchlist" component={WatchList}/>
+              <Route path="/reviews" component={Reviews}/>
               <Redirect to="/"/>
             </Switch>
+            }
           </div>
         </main>
       </Router>
@@ -32,4 +53,7 @@ class App extends PureComponent {
   }
 }
 
-export default App;
+export default connect(
+  state => ({ checkedAuth: getCheckedAuth(state) }),
+  { tryLoadUser }
+)(App);
