07351af8e1bca3a25b82c6363fa46bd2bd3275d7
Jen Lipton <jkl.lipton@gmail.com>
2018-06-26 11:37:27 -0700

    Create Home.js

---
+++ src/components/home/Home.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+
+class Home extends PureComponent {
+  
+  render() {
+    return (
+      <div>
+        <p>Home Page</p>      
+      </div>
+    );
+  }
+}
+
+export default Home;
