e07de76622e2d6b697e35e638211c44b053abd7f
RA3light <desertjynx@gmail.com>
2018-06-25 17:35:28 -0700

    mergeeeeeeee

---
+++ src/components/reviews/Reviews.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Reviews extends Component {
+  render() {
+    return (
+      <div>
+        <p>I am the reviews</p>
+      </div>
+    );
+  }
+}
+
+export default Reviews;
