5fc6afd9a783e82b478642c5876bf613224ff94c
Jen Lipton <jkl.lipton@gmail.com>
2018-06-26 11:38:24 -0700

    Create Profile.js

---
+++ src/components/profile/Profile.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Profile extends Component {
+  render() {
+    return (
+      <div>
+        <p>I am a profile</p>
+      </div>
+    );
+  }
+}
+
+export default Profile;
