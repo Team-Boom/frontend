d4b9d626ce5f9be8095c11da29b8420693da84eb
Jen Lipton <jkl.lipton@gmail.com>
2018-06-26 13:24:35 -0700

    constants file

---
+++ src/components/shared/constants.js
@@ -0,0 +1,9 @@
+export const categories = ['Cinematography', 'Editing', 'Design', 'Lighting', 'Sound'];
+
+export const categoryBlurbs = {
+  'Cinematography': 'Cinematography blurb',
+  'Editing': 'Editing blurb',
+  'Design': 'Design blurb',
+  'Lighting': 'Lighting blurb',
+  'Sound': 'Sound blurb'
+};
