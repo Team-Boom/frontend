1613a4bcb91af6448fa3602294590d492928892b
Jen Lipton <jkl.lipton@gmail.com>
2018-06-27 16:57:36 -0700

    mergggeee

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,11 +1,14 @@
 import React, { PureComponent } from 'react';
+import Carousel from '../shared/Carousel';
 
 class Home extends PureComponent {
   
   render() {
     return (
       <div>
-        <p>Home Page</p>      
+        <p>Home Page</p>     
+        <Carousel />
+        <p>I am Browse</p>       
       </div>
     );
   }
