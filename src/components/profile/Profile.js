b7db51655307899d41984d8f06eba50b0649200e
Tasha R Zuniga <Tashazun@gmail.com>
2018-06-28 15:00:41 -0700

    put logout in profile and added grid/media queries to header

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { getUser } from '../profile/reducers';
+import { logout } from '../profile/actions';
+import PropTypes from 'prop-types';
 
 class Profile extends Component {
+  static propTypes = {
+    user: PropTypes.object,
+    logout: PropTypes.func.isRequired
+  }
+
+  handleLogout = () => {
+    this.props.logout();
+  }
+  
   render() {
+    const { user } = this.props;
     return (
       <div>
         <p>I am a profile</p>
+        <div id='links'>
+          {
+            user
+              ? <Link to="/" onClick={this.handleLogout}>Logout</Link>
+              : <Link to="/auth">Login</Link>
+          } 
+        </div>
       </div>
     );
   }
 }
 
-export default Profile;
+export default connect(
+  state => ({ user: getUser(state) }),
+  { logout }
+)(Profile);
--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -1,50 +1,26 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import styles from './Header.scss';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { getUser } from '../profile/reducers';
-import { logout } from '../profile/actions';
 import SearchBar from './SearchBar';
 import logo from '../../assets/images/DeepFocus.png';
 
-class Header extends Component {
+export default class Header extends Component {
 
     
-  static propTypes = {
-    user: PropTypes.object,
-    logout: PropTypes.func.isRequired
-  }
 
-  handleLogout = () => {
-    this.props.logout();
-  }
 
   render() {
 
-    const { user } = this.props;
 
     return (
       <div className={styles.header}>
-        <div id='box1'>
+        <div id='box'>
           <img id='logo' src={logo}/>
         </div>
         <div id='search'>
           <SearchBar />
-        </div>
-        <div>
-          {
-            user
-              ? <Link to="/" onClick={this.handleLogout}>Logout</Link>
-              : <Link to="/auth">Login</Link>
-          } 
+
         </div>
       </div>
     );
   }
 }
-
-export default connect(
-  state => ({ user: getUser(state) }),
-  { logout }
-)(Header);
